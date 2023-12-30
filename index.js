ac6eed87985e39b0fbcd26c71aadba45418079b9
shiba <rp.shiba2@gmail.com>
2023-12-30 17:56:59 +0530

    post  req

---
+++ index.js
@@ -0,0 +1,21 @@
+//for company website
+const express = require('express');
+var cors = require("cors");
+require("dotenv").config();
+const app = express();
+app.use(cors());//to handle cors error 
+app.use(express.json());//This middleware parses JSON in the request body
+const port = process.env.PORT || 3000;
+
+
+
+app.post('/contact', (req, res) => {
+    const { email, subject, message } = req.body;
+    console.log('Received data:', { email, subject, message });
+    res.status(200).json({ success: true, message: 'Data received successfully' });
+});
+
+
+app.listen(port, () => {
+  console.log(`server is running ${port}` );
+});
